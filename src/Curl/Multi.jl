a3c5780f660898d1ee41d5011100219764c2f1b7
Stefan Karpinski <stefan@karpinski.org>
2020-09-10 13:57:16 -0400

    fix laziness from libcurl/libuv example code
    In https://curl.haxx.se/libcurl/c/multi-uv.html the defintion of
    start_timeout has the following code with a suspicious comment:
    
        if(timeout_ms == 0)
          timeout_ms = 1; /* 0 means directly call socket_action,
                             but we'll do it in a bit */
    
    It turns out that if libcurl fetches a URL and the fetch never needs
    to yield to the scheduler, this causes the download to hang until
    some other event (like a key press) triggers the scheduler. This was
    discovered while implementing and testing proper error handling. The
    case where this hangs is a URL where libcurl knows before even trying
    to talk to a server that the download will fail, such as one with an
    invalid or unsupported protocol. In such a case, without this fix,
    the download call would hang until you press a key or something else
    happens to trigger a libuv event.

--- src/Curl/Multi.jl
+++ src/Curl/Multi.jl
@@ -76,9 +76,12 @@ function timer_callback(
 )::Cint
     multi = unsafe_pointer_to_objref(multi_p)::Multi
     @assert handle_p == multi.handle
-    if timeout_ms ≥ 0
+    if timeout_ms == 0
+        @check curl_multi_socket_action(multi.handle, CURL_SOCKET_TIMEOUT, 0)
+        check_multi_info(multi)
+    elseif timeout_ms > 0
         timeout_cb = @cfunction(timeout_callback, Cvoid, (Ptr{Cvoid},))
-        uv_timer_start(multi.timer, timeout_cb, max(1, timeout_ms), 0)
+        uv_timer_start(multi.timer, timeout_cb, timeout_ms, 0)
     else
         uv_timer_stop(multi.timer)
     end
