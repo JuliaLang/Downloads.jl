6ac80cfb7f8e0a047d94687f92926d59c09730ab
Stefan Karpinski <stefan@karpinski.org>
2020-10-07 17:28:40 -0400

    prevent deadlocks: call yield at start of download

--- src/Downloads.jl
+++ src/Downloads.jl
@@ -45,6 +45,7 @@ function download(
     headers::Headers = Pair{String,String}[],
     downloader::Downloader = default_downloader(),
 )
+    yield() # prevents deadlocks, shouldn't be necessary
     arg_write(output) do io
         easy = Easy()
         set_url(easy, url)
@@ -87,6 +88,7 @@ struct Response
 end
 
 function request(req::Request, multi = Multi(), progress = p -> nothing)
+    yield() # prevents deadlocks, shouldn't be necessary
     easy = Easy()
     set_url(easy, req.url)
     for hdr in req.headers
