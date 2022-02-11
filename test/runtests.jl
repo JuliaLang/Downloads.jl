b08c869f9d15fbe8e18ac8a344a40df2183ce172
Elliot Saba <staticfloat@gmail.com>
2022-02-10 18:53:53 -0800

    Use self-hosted `badssl.julialang.org` server (#176)
    This should hopefully allow us to avoid flooding the `badssl.com` people
    with our CI requests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -469,8 +469,8 @@ include("setup.jl")
 
     @testset "bad TLS" begin
         urls = [
-            "https://untrusted-root.badssl.com"
-            "https://wrong.host.badssl.com"
+            "https://untrusted-root.badssl.julialang.org"
+            "https://wrong.host.badssl.julialang.org"
         ]
         @testset "bad TLS is rejected" for url in urls
             resp = request(url, throw=false)
@@ -504,7 +504,7 @@ include("setup.jl")
             Downloads.DOWNLOADER[] = nothing
             Downloads.EASY_HOOK[] = nothing
         end
-        ENV["JULIA_SSL_NO_VERIFY_HOSTS"] = "**.badssl.com"
+        ENV["JULIA_SSL_NO_VERIFY_HOSTS"] = "**.badssl.julialang.org"
         # wrong host *should* still fail, but may not due
         # to libcurl bugs when using non-OpenSSL backends:
         pop!(urls) # <= skip wrong host URL entirely here
