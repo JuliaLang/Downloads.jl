89ae98f1f6b7c96a45030da58e3375f6f2205ac6
tan <tanmaykm@gmail.com>
2024-09-05 05:24:26 +0530

    fix cancelling upload requests
    Cancelling an upload (PUT) request using the mechanism introduced in #256 was not effective. The upload task was not interrupted, which still blocked and the call to `request` did not return.
    With this change, cancelling also closes the `input` stream of the request to unblock the upload task.
    
    Also changed the `interrupted` variable to be an `Atomic{Bool}`. Ref discussion [here](https://github.com/JuliaLang/Downloads.jl/pull/256#discussion_r1742148570).

--- test/runtests.jl
+++ test/runtests.jl
@@ -478,6 +478,17 @@ include("setup.jl")
             timedwait(()->istaskdone(download_task), 5.0)
             @test istaskdone(download_task)
             @test download_task.result isa RequestError
+
+            interrupt = Base.Event()
+            url = "$server/put"
+            input=`sh -c 'sleep 15; echo "hello"'`
+            download_task = @async request(url; interrupt=interrupt, input=input)
+            sleep(0.1)
+            @test !istaskdone(download_task)
+            notify(interrupt)
+            timedwait(()->istaskdone(download_task), 5.0)
+            @test istaskdone(download_task)
+            @test download_task.result isa RequestError
         end
 
         @testset "progress" begin
